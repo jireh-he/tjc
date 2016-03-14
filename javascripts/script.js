2fcf5c183c844e48c54692f6d21240fe0d566ad0
jireh <18772119@qq.com>
2016-03-14 10:34:08 +0800

    Update script.js

--- javascripts/script.js
+++ javascripts/script.js
@@ -1,52 +1,235 @@
-(function($) {
-$(document).ready(function(){
+(function($){
+	// Settings
+	var repeat = localStorage.repeat || 0,
+		shuffle = localStorage.shuffle || 'false',
+		continous = true,
+		autoplay = true,
+		playlist=[],
+		homepage='http://www.truejesuschurch.info';
 
-  // putting lines by the pre blocks
-  $("pre").each(function(){
-    var pre = $(this).text().split("\n");
-    var lines = new Array(pre.length+1);
-    for(var i = 0; i < pre.length; i++) {
-      var wrap = Math.floor(pre[i].split("").length / 70)
-      if (pre[i]==""&&i==pre.length-1) {
-        lines.splice(i, 1);
-      } else {
-        lines[i] = i+1;
-        for(var j = 0; j < wrap; j++) {
-          lines[i] += "\n";
-        }
-      }
-    }
-    $(this).before("<pre class='lines'>" + lines.join("\n") + "</pre>");
-  });
+		//这是一个非常简单的demo实例，让列表元素分页显示
+	//回调函数的作用是显示对应分页的列表项内容
+	//回调函数在用户每次点击分页链接的时候执行
+	//参数page_index{int整型}表示当前的索引页
+	var initPagination = function() {
+		var num_entries = medialist.length;
+		// 创建分页
+		$("#Pagination").pagination(num_entries, {
+			num_edge_entries: 1, //边缘页数
+			num_display_entries: 4, //主体页数
+			callback: pageselectCallback,
+			items_per_page:10 //每页显示10项
+		});
+	 }();
 
-  var headings = [];
+	function pageselectCallback(page_index, jq){
+		playlist = medialist.slice(page_index*10,page_index*10+10);
+		$("#playlist").empty();
+		for (var i=0;i<playlist.length;i++){
+			var item = playlist[i];
+			$("#playlist").append('<li>'+item.title+'</li>'); //装载对应分页的内容
+		}
 
-  var collectHeaders = function(){
-    headings.push({"top":$(this).offset().top - 15,"text":$(this).text()});
-  }
+		return false;
+	}
 
-  if($(".markdown-body h1").length > 1) $(".markdown-body h1").each(collectHeaders)
-  else if($(".markdown-body h2").length > 1) $(".markdown-body h2").each(collectHeaders)
-  else if($(".markdown-body h3").length > 1) $(".markdown-body h3").each(collectHeaders)
 
-  $(window).scroll(function(){
-    if(headings.length==0) return true;
-    var scrolltop = $(window).scrollTop() || 0;
-    if(headings[0] && scrolltop < headings[0].top) {
-      $(".current-section").css({"opacity":0,"visibility":"hidden"});
-      return false;
-    }
-    $(".current-section").css({"opacity":1,"visibility":"visible"});
-    for(var i in headings) {
-      if(scrolltop >= headings[i].top) {
-        $(".current-section .name").text(headings[i].text);
-      }
-    }
-  });
+	var time = new Date(),
+		currentTrack = shuffle === 'true' ? time.getTime() % playlist.length : 0,
+		trigger = false,
+		audio, timeout, isPlaying, playCounts;
 
-  $(".current-section a").click(function(){
-    $(window).scrollTop(0);
-    return false;
-  })
-});
-})(jQuery)
+	var play = function(){
+		audio.play();
+		$('.playback').addClass('playing');
+		timeout = setInterval(updateProgress, 500);
+		isPlaying = true;
+	}
+
+	var pause = function(){
+		audio.pause();
+		$('.playback').removeClass('playing');
+		clearInterval(updateProgress);
+		isPlaying = false;
+	}
+
+	// Update progress
+	var setProgress = function(value){
+		var currentSec = parseInt(value%60) < 10 ? '0' + parseInt(value%60) : parseInt(value%60),
+			ratio = value / audio.duration * 100;
+
+		$('.timer').html(parseInt(value/60)+':'+currentSec);
+		$('.progress .pace').css('width', ratio + '%');
+		$('.progress .slider a').css('left', ratio + '%');
+	}
+
+	var updateProgress = function(){
+		setProgress(audio.currentTime);
+	}
+
+	// Progress slider
+	$('.progress .slider').slider({step: 0.1, slide: function(event, ui){
+		$(this).addClass('enable');
+		setProgress(audio.duration * ui.value / 100);
+		clearInterval(timeout);
+	}, stop: function(event, ui){
+		audio.currentTime = audio.duration * ui.value / 100;
+		$(this).removeClass('enable');
+		timeout = setInterval(updateProgress, 500);
+	}});
+
+	// Volume slider
+	var setVolume = function(value){
+		audio.volume = localStorage.volume = value;
+		$('.volume .pace').css('width', value * 100 + '%');
+		$('.volume .slider a').css('left', value * 100 + '%');
+	}
+
+	var volume = localStorage.volume || 0.5;
+	$('.volume .slider').slider({max: 1, min: 0, step: 0.01, value: volume, slide: function(event, ui){
+		setVolume(ui.value);
+		$(this).addClass('enable');
+		$('.mute').removeClass('enable');
+	}, stop: function(){
+		$(this).removeClass('enable');
+	}}).children('.pace').css('width', volume * 100 + '%');
+
+	$('.mute').click(function(){
+		if ($(this).hasClass('enable')){
+			setVolume($(this).data('volume'));
+			$(this).removeClass('enable');
+		} else {
+			$(this).data('volume', audio.volume).addClass('enable');
+			setVolume(0);
+		}
+	});
+
+	// Switch track
+	var switchTrack = function(i){
+		if (i < 0){
+			track = currentTrack = playlist.length - 1;
+		} else if (i >= playlist.length){
+			track = currentTrack = 0;
+		} else {
+			track = i;
+		}
+
+		$('audio').remove();
+		loadMusic(track);
+		if (isPlaying == true) play();
+	}
+
+	// Shuffle
+	var shufflePlay = function(){
+		var time = new Date(),
+			lastTrack = currentTrack;
+		currentTrack = time.getTime() % playlist.length;
+		if (lastTrack == currentTrack) ++currentTrack;
+		switchTrack(currentTrack);
+	}
+
+	// Fire when track ended
+	var ended = function(){
+		pause();
+		audio.currentTime = 0;
+		playCounts++;
+		if (continous == true) isPlaying = true;
+		if (repeat == 1){
+			play();
+		} else {
+			if (shuffle === 'true'){
+				shufflePlay();
+			} else {
+				if (repeat == 2){
+					switchTrack(++currentTrack);
+				} else {
+					if (currentTrack < playlist.length) switchTrack(++currentTrack);
+				}
+			}
+		}
+	}
+
+	var beforeLoad = function(){
+		var endVal = this.seekable && this.seekable.length ? this.seekable.end(0) : 0;
+		$('.progress .loaded').css('width', (100 / (this.duration || 1) * endVal) +'%');
+	}
+
+	// Fire when track loaded completely
+	var afterLoad = function(){
+		if (autoplay == true) play();
+	}
+
+	// Load track
+	var loadMusic = function(i){
+		var item = playlist[i],
+			newaudio = $('<audio>').html('<source src="'+homepage+item.mp3+'">').appendTo('#player');
+			$('.cover').html('<img src="'+item.cover+'" alt="'+item.title+'">');
+		$('.tag').html('<strong>'+item.title+'</strong>');
+		$('#playlist li').removeClass('playing').eq(i).addClass('playing');
+		audio = newaudio[0];
+		audio.volume = $('.mute').hasClass('enable') ? 0 : volume;
+		audio.addEventListener('progress', beforeLoad, false);
+		audio.addEventListener('durationchange', beforeLoad, false);
+		audio.addEventListener('canplay', afterLoad, false);
+		audio.addEventListener('ended', ended, false);
+	}
+
+	loadMusic(currentTrack);
+	$('.playback').on('click', function(){
+		if ($(this).hasClass('playing')){
+			pause();
+		} else {
+			play();
+		}
+	});
+	$('.rewind').on('click', function(){
+		if (shuffle === 'true'){
+			shufflePlay();
+		} else {
+			switchTrack(--currentTrack);
+		}
+	});
+	$('.fastforward').on('click', function(){
+		if (shuffle === 'true'){
+			shufflePlay();
+		} else {
+			switchTrack(++currentTrack);
+		}
+	});
+	$('#playlist li').each(function(i){
+		var _i = i;
+		$(this).on('click', function(){
+			switchTrack(_i);
+		});
+	});
+
+	if (shuffle === 'true') $('.shuffle').addClass('enable');
+	if (repeat == 1){
+		$('.repeat').addClass('once');
+	} else if (repeat == 2){
+		$('.repeat').addClass('all');
+	}
+
+	$('.repeat').on('click', function(){
+		if ($(this).hasClass('once')){
+			repeat = localStorage.repeat = 2;
+			$(this).removeClass('once').addClass('all');
+		} else if ($(this).hasClass('all')){
+			repeat = localStorage.repeat = 0;
+			$(this).removeClass('all');
+		} else {
+			repeat = localStorage.repeat = 1;
+			$(this).addClass('once');
+		}
+	});
+
+	$('.shuffle').on('click', function(){
+		if ($(this).hasClass('enable')){
+			shuffle = localStorage.shuffle = 'false';
+			$(this).removeClass('enable');
+		} else {
+			shuffle = localStorage.shuffle = 'true';
+			$(this).addClass('enable');
+		}
+	});
+})(jQuery);
